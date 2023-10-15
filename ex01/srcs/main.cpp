1e49dc171e5c5b4375e64b9d74caa27e303f1fae
dorakueYaritai <kotto555555@gmail.com>
2023-10-15 18:44:47 +0900

    0除算 符号過多

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -25,20 +25,39 @@ int	calculate(int front, int back, char ope) {
 	else if (ope == '*')
 		return (front * back);
 	else if (ope == '/')
+	{
+		if (back == 0)
+			throw std::exception();
 		return (front / back);
+	}
 	return 0;
 }
 
 bool	tokenize(const std::string& str, std::queue<std::string> &tokens) {
-    std::string token;
-    std::istringstream tokenStream(str);
+    std::string			token;
+    std::istringstream	tokenStream(str);
+	int					acceptableOperator = -1;
+
     while (std::getline(tokenStream, token, ' ')) {
-		if (token.size() != 1 || 
-			(!isoperator(token[0])&& !std::isdigit(token[0])))
-			return (false);
-		else
+		if (token.size() != 1)
+			return false;
+		else if (isoperator(token[0]))
+		{
+			if (acceptableOperator < 1)
+				return false;
+			acceptableOperator--;
 			tokens.push(token);
+		}
+		else if (std::isdigit(token[0]))
+		{
+			acceptableOperator++;
+			tokens.push(token);
+		}
+		else
+			return false;
     }
+	if (acceptableOperator != 0)
+		return false;
 	return (true);
 }
 
@@ -73,6 +92,13 @@ int	main(int argc, char **argv) {
 		std::cout << "ERROR" << std::endl;
 		return 1;
 	}
-	std::cout << evaluateRPN(tokens) << std::endl;
+	try {
+		std::cout << evaluateRPN(tokens) << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "ERROR" << std::endl;
+		return (1);
+	}
 	return 0;
 }

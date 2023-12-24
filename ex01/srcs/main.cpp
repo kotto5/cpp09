7a1cde5c8f56bd490077b6d44dce41ca7d36f28e
kotto5 <kotto555555@gmail.com>
2023-12-24 10:58:10 +0900

    add tester

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -1,25 +1,49 @@
 #include "RPN.hpp"
-// int	tester(std::string &str) {
-// 	std::queue<std::string>	tokens;
 
-// 	if (!tokenize(str, tokens))
-// 	{
-// 		std::cout << "ERROR" << std::endl;
-// 		return 1;
-// 	}
-// 	try {
-// 		std::cout << evaluateRPN(tokens) << std::endl;
-// 	}
-// 	catch (std::exception &e)
-// 	{
-// 		std::cout << "ERROR" << std::endl;
-// 		return (1);
-// 	}
-// 	return 0;
-// }
+#define GREEN "\033[32m"
+#define RED "\033[31m"
+#define RESET "\033[0m"
+
+int	putResult(bool success, Result<int> result) {
+	if (success)
+	{
+		std::cerr << GREEN;
+		std::cout << "OK : ";
+		result.isOk() ? std::cerr << result.getResult() : std::cerr << "ERROR";
+		std::cerr << RESET << std::endl;
+		return 0;
+	}
+	else
+	{
+		std::cerr << RED;
+		std::cerr << "ERROR: getResult() returned: ";
+		result.isOk() ? std::cerr << result.getResult() : std::cerr << "ERROR";
+		std::cerr << RESET << std::endl;
+		return 0;
+	}
+}
+
+
+
+int	tester(std::string str, bool success, int expect = 0) {
+	std::cerr << GREEN << "Testing \"" << str << "\" Expect: ";
+	success ? std::cerr << expect : std::cerr << "ERROR";
+	std::cerr << RESET << std::endl;
+
+	Result<int> result = RPN::evaluate(str);
+	if (result.isOk() != success || (!success && result.getResult() != ERROR))
+		return putResult(false, result);
+	else
+		return putResult(true, result);
+}
 
 
 int	main(int argc, char **argv) {
+	tester("1 2 +", true, 3);
+	tester("2 2 +", true, 4);
+	tester("+ +", false);
+	tester("+ +", true, 0);
+	tester("1 + +", false, 0);
 	if (argc == 1)
 	{
 		std::cout << "ERROR" << std::endl;
